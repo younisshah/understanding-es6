63767ad411954c508a774944d35fb2442008c056
Younis Shah <younisashah@gmail.com>
2016-03-30 13:56:28 +0530

    ES5 implementation

--- spread.js
+++ spread.js
@@ -9,3 +9,13 @@ function foo(i, j, k, l){
 }
 
 foo(...list); // 24
+
+// ES5 Implementation
+
+var list = [1, 2, 3, 4];
+
+function foo(i, j, k, l) {
+    return i * j * k * l;
+}
+
+foo.apply(undefined, list); // 24
